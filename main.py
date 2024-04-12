4f8c912d6651908c57b618f228ceddac3fc26148
Hizrok <jan.kapsa@seznam.cz>
2024-04-12 22:37:42 +0200

    feat: keypoint map redesign

--- main.py
+++ main.py
@@ -1,135 +1,198 @@
 #!/usr/bin/python3
 
-import cv2 as cv
+import cv2
 import numpy as np
-import math
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, Point
 
-DIR = "./data/images/DJI_{}.jpg"
+DIR = "./data/images2/DJI_{}.JPG"
 
-TOTAL = 20
-OFFSET = 1000
+# SIFT object
+sift = cv2.SIFT_create()
 
-def run():
-	image_a = cv.imread(get_image_path(1))
+# FLANN matcher
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+class Keypoint:
+	def __init__(self, index, point, descriptor):
+		self.index = index
+		self.point = point
+		self.descriptor = descriptor
+
+class Map:
+	def __init__(self):
+		self.keypoints: list[Keypoint] = []
+
+	def add_keypoints(self, new_keypoints: list[Keypoint]):
+		self.keypoints += new_keypoints
+
+	def get_keypoints(self, polygon: Polygon):
+		keypoints: list[Keypoint] = []
+
+		for keypoint in self.keypoints:
+			point = Point(keypoint.point)
+			if polygon.contains(point):
+				keypoints.append(keypoint)
+
+		return keypoints
 	
-	height, width = image_a.shape[:2]
-	offset_x = (width+OFFSET) / 2 - width / 2
-	offset_y = (height+OFFSET) / 2 - height / 2
-	translation_matrix = np.float32([[1, 0, offset_x], [0, 1, offset_y], [0, 0, 1]])
-	res = (width+OFFSET, height+OFFSET)
-	rect = get_rect(image_a, translation_matrix)
-	image_a = cv.warpPerspective(image_a, translation_matrix, res)
+class Generator:
+	def __init__(self):
+		self.map = Map()
 
-	for i in range(2, TOTAL+1):	
-		print(f"[{i}] detecting features from the main image")
-		key_points_a, descriptor_a = detect_features(image_a)
-		image_b = cv.imread(get_image_path(i))
+		self.incremental_bboxes = []
+		self.map_bboxes = []
+
+	def run(self):
 		
-		print(f"[{i}] detecting features from the new image")
-		key_points_b, descriptor_b = detect_features(image_b)
+		prev_features, prev_descriptors, prev_homography, prev_bbox = self.initialize()
+
+		self.incremental_bboxes.append(prev_bbox)
+		self.map_bboxes.append(prev_bbox)
+
+		for index in range(21, 31):
+			print(index)
+			prev_features, prev_descriptors, prev_homography = self.incremental_new_image(index, prev_features, prev_descriptors, prev_homography)
+			prev_bbox = self.map_new_image(index, prev_bbox)
+
+		self.plot_bboxes(self.incremental_bboxes, self.map_bboxes)
+
+		pass
+
+	def initialize(self):
+		img = cv2.imread(get_image_path(20))[::4,::4]
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		features, descriptors = sift.detectAndCompute(gray, None)
+		homography = [[1,0,0], [0,1,0], [0,0,1]]
+
+		h, w = img.shape[:2]
+		bbox = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+
+		keypoints = [Keypoint(1, features[i].pt, descriptors[i]) for i in range(len(features))]
+
+		self.map.add_keypoints(keypoints)
+
+		return features, descriptors, homography, bbox
+
+	def incremental_new_image(self, index, prev_features, prev_descriptors, prev_homography):
+		img = cv2.imread(get_image_path(index))[::4,::4]
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		features, descriptors = sift.detectAndCompute(gray, None)
+
+		h, w = img.shape[:2]
+		bbox = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+
+		matches = find_matches(prev_descriptors, descriptors)
+
+		src_pts = [features[m.trainIdx].pt for m in matches]
+		dst_pts = [prev_features[m.queryIdx].pt for m in matches]
+
+		homography = find_homography(src_pts, dst_pts)
+		homography = np.matmul(prev_homography, homography)
+
+		transformed_bbox = cv2.perspectiveTransform(bbox.reshape(-1, 1, 2), homography)
+		transformed_bbox = np.squeeze(transformed_bbox)
+		self.incremental_bboxes.append(transformed_bbox)
 		
-		print(f"[{i}] matching features")
-		matches = match_features(descriptor_a, descriptor_b)
+		return features, descriptors, homography
+	
+	def map_new_image(self, index, prev_bbox):
+		img = cv2.imread(get_image_path(index))[::4,::4]
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		features, descriptors = sift.detectAndCompute(gray, None)
+
+		h, w = img.shape[:2]
+		bbox = np.array([[0, 0], [w, 0], [w, h], [0, h]], dtype=np.float32)
+
+		# enlarged_bbox = prediction window
+		enlarged_bbox = []
+		polygon = Polygon(prev_bbox)
+		centroid = np.array([polygon.centroid.x, polygon.centroid.y])
 		
-		print(f"[{i}] calculating homography")
-		homography = get_homography(key_points_a, key_points_b, matches)
-
-		print(f"[{i}] warping image")
-		warped_image = cv.warpPerspective(image_b, homography, (image_a.shape[1], image_a.shape[0]))
-
-		overlayed_image = np.where(warped_image != 0, warped_image, image_a)
-		image_a = overlayed_image
-
-		print(f"rect: {rect}")
-		new_rect = get_rect(image_b, homography)
-		print(f"new rect: {new_rect}")
+		for point in prev_bbox:
+			vector = np.array([point[0] - centroid[0], point[1] - centroid[1]])
+			norm = np.linalg.norm(vector)
+			norm_vector = vector / norm
+			dist = np.linalg.norm(point - centroid)
+			enlarged_bbox.append(centroid + (norm_vector * dist * 1.2))
+		enlarged_bbox = np.array(enlarged_bbox, dtype=np.float32)
 		
-		rect = merge_rects(rect, new_rect)
-		print(f"merged: {rect}")
-		
-		print(f"old res: {res}")
-		res = get_new_res(rect)
-		print(f"new_res: {res}")
-		
-		image_a, rect = center(image_a, rect, res)
+		keypoints: list[Keypoint] = self.map.get_keypoints(Polygon(enlarged_bbox))
+		map_points = [keypoints[i].point for i in range(len(keypoints))]
+		map_descriptors = np.float32([keypoints[i].descriptor for i in range(len(keypoints))])
 
-		cv.imwrite(f"out.jpg", image_a)
-		# input("Press Enter to continue...")
+		matches = find_matches(map_descriptors, descriptors)
 
-def center(image, rect, res):
-	min_x, min_y, max_x, max_y = rect
-	w, h = (max_x-min_x, max_y-min_y)
-	W, H = res
+		src_pts = [features[m.trainIdx].pt for m in matches]
+		dst_pts = [map_points[m.queryIdx] for m in matches]
 
-	offset_x = round(W / 2 - w / 2 - min_x)
-	offset_y = round(H / 2 - h / 2 - min_y)
-	translation_matrix = np.float32([[1, 0, offset_x], [0, 1, offset_y]])
-	warped_image = cv.warpAffine(image, translation_matrix, res)
-	new_rect = (min_x+offset_x, min_y+offset_y, max_x+offset_x, max_y+offset_y)
-	return warped_image, new_rect
+		homography = find_homography(src_pts, dst_pts)
 
-def get_new_res(rect):
-	min_x, min_y, max_x, max_y = rect
-	width = max_x-min_x
-	height = max_y-min_y
-	return (width+OFFSET, height+OFFSET)
+		transformed_bbox = cv2.perspectiveTransform(bbox.reshape(-1, 1, 2), homography)
+		transformed_bbox = np.squeeze(transformed_bbox)
+		self.map_bboxes.append(transformed_bbox)
 
-def get_rect(image, matrix):
-	height, width = image.shape[:2]
-	corners = np.array([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]], dtype=np.float32).reshape(-1, 1, 2)
-	warped_corners = cv.perspectiveTransform(corners, matrix)
-	min_x = round(np.min(warped_corners[:, :, 0]))
-	min_y = round(np.min(warped_corners[:, :, 1]))
-	max_x = round(np.max(warped_corners[:, :, 0])+1)
-	max_y = round(np.max(warped_corners[:, :, 1])+1)
-	return (min_x, min_y, max_x, max_y)
+		# TODO: only add points that are not matches
+		new_points = np.array([kp.pt for kp in features])
+		new_points = cv2.perspectiveTransform(new_points.reshape(-1, 1, 2), homography)
+		new_points = np.squeeze(new_points)
 
-def merge_rects(rect_a, rect_b):
-	a_min_x, a_min_y, a_max_x, a_max_y = rect_a
-	b_min_x, b_min_y, b_max_x, b_max_y = rect_b
-	min_x = min(a_min_x, b_min_x)
-	min_y = min(a_min_y, b_min_y)
-	max_x = max(a_max_x, b_max_x)
-	max_y = max(a_max_y, b_max_y)
-	return (min_x, min_y, max_x, max_y)
+		self.map.add_keypoints([Keypoint(index, new_points[i], descriptors[i]) for i in range(len(descriptors))])
 
-def get_homography(key_points_a, key_points_b, matches):
-	# Extract keypoints from the good matches
-	src_pts = np.float32([key_points_a[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-	dst_pts = np.float32([key_points_b[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+		return transformed_bbox
 
-	# Find homography
-	homography, _ = cv.findHomography(dst_pts, src_pts, cv.RANSAC)
+	def plot_bboxes(self, bboxes1, bboxes2):
+		min_y = float('inf')
+		max_y = float('-inf')
+		for i in range(len(bboxes1)):
+			bbox1 = bboxes1[i]
+			bbox2 = bboxes2[i]
+			
+			plot_bbox(bboxes1[i], 'red')
+			plot_bbox(bboxes2[i], 'blue')
+			
+			min_y_bbox1 = np.min(bbox1[:, 1])
+			max_y_bbox1 = np.max(bbox1[:, 1])
+			min_y_bbox2 = np.min(bbox2[:, 1])
+			max_y_bbox2 = np.max(bbox2[:, 1])
+			
+			min_y = min(min_y, min_y_bbox1, min_y_bbox2)
+			max_y = max(max_y, max_y_bbox1, max_y_bbox2)
+		plt.ylim(max_y+50, min_y-50)
+		plt.show()
+	
+def get_min_max(bbox):
+	min_x = np.min(bbox[:, 0])
+	max_x = np.max(bbox[:, 0])
+	min_y = np.min(bbox[:, 1])
+	max_y = np.max(bbox[:, 1])
 
-	return homography
+	return (min_x, max_x, min_y, max_y)
 
-def match_features(descriptor_a, descriptor_b):
-	flann = cv.FlannBasedMatcher()
-	matches = flann.knnMatch(descriptor_a, descriptor_b, k=2)
+def plot_bbox(bbox, color='red'):
+	for i in range(len(bbox)):
+		current_point = bbox[i]
+		next_point = bbox[(i + 1) % len(bbox)]
+		plt.plot([current_point[0], next_point[0]], [current_point[1], next_point[1]], '-', color=color)
+
+def find_matches(prev_descriptors, new_descriptors):
+	matches = flann.knnMatch(prev_descriptors, new_descriptors, k=2)
 	good_matches = []
 	for m, n in matches:
 		if m.distance < 0.7 * n.distance:
 			good_matches.append(m)
-	# print(len(good_matches))
 	return good_matches
 
-def detect_features(image):
-	gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-	sift = cv.SIFT_create()
-	key_points, descriptor = sift.detectAndCompute(gray, None)
-	return (key_points, descriptor)
+def find_homography(src_pts, dst_pts):
+	src_pts = np.float32(src_pts).reshape(-1, 1, 2)
+	dst_pts = np.float32(dst_pts).reshape(-1, 1, 2)
 
-def resize_image(image, factor=2):
-	height, width = image.shape[:2]
-	new_height = int(height / factor)
-	new_width = int(width / factor)
-	resized_image = cv.resize(image, (new_width, new_height))
-	return resized_image
+	H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-def show_image(image, description="image"):
-	cv.imshow(description, image)
-	cv.waitKey(0)
-	cv.destroyAllWindows()
+	return H
 
 def get_image_path(index):
 	str_index = str(index)
@@ -138,4 +201,6 @@ def get_image_path(index):
 	return DIR.format(str_index)
 
 if __name__ == "__main__":
-	run()
+	# run()
+	g = Generator()
+	g.run()
